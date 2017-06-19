c710899f9c8c5d13c1e6a39470a330026c5ecad5
Chris Miller <chrisamiller@gmail.com>
2017-06-19 13:38:44 -0500

    adding missing files

---
+++ split_interval_list_helper.pl
@@ -0,0 +1,11 @@
+use File::Copy;
+
+my $retval = system('/usr/bin/java', '-jar', '/usr/picard/picard.jar', 'IntervalListTools', @ARGV);
+exit $retval if $retval != 0;
+
+my $i = 1;
+for(glob('*/scattered.interval_list')) {
+    #create unique names and relocate all the scattered intervals to a single directory
+    File::Copy::move($_, qq{$i.interval_list});
+    $i++
+}
